f33ea522984cbbec0768c887d2f771a09626188e
Sajad Abbasi <sajjadintel@gmail.com>
2021-02-28 19:57:55 +0500

    ONEX-339

--- assets/js/dt/inquiries.js
+++ assets/js/dt/inquiries.js
@@ -171,14 +171,14 @@ var KTDatatableInquiry = (function() {
 					template: function(row) {
 						let emailNeeded = '';
 						if(row.noOfRcverUsers==0){
-							emailNeeded = '<a href="javascript:;" class="btn btn-outline-primary ml-5 fa fa-envelope" title="Add Email" onclick="KTDatatableInquiry.addEmail('+ row.messageId +')"></a>';
+							emailNeeded += '<a href="javascript:;" class="btn btn-outline-primary ml-5 fa fa-envelope" title="Add Email" onclick="KTDatatableInquiry.addEmail('+ row.messageId +')"></a>';
 						}
 						var temp='<a href="javascript:;" class="btn btn-outline-secondary fab fa-readme" title="View Inquiry" onclick="KTDatatableInquiry.viewMessage('+ row.messageId +')"></a>';
 						if(row.actionStatus == 0 && row.parentId == 0){
 							temp += '<a href="javascript:;" class="btn btn-outline-primary ml-5" title="Approve" onclick="KTDatatableInquiry.approveMessage('+ row.messageId +')"> <i class="ki ki-bold-check-1 icon-sm"></i></a>' +
 								'<a href="javascript:;" class="btn btn-outline-danger ml-5" title="Disapprove" onclick="KTDatatableInquiry.disapproveMessage('+ row.messageId +')"> <i class="ki ki-bold-close icon-sm"></a>';
 						}
-						temp = emailNeeded;
+						temp += emailNeeded;
 						return temp;
 
 						return (
