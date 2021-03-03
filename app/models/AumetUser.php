bfea89a4205f371d30ddf8879140e2487ffa235f
Sajad Abbasi <sajjadintel@gmail.com>
2021-03-04 01:33:31 +0400

    USE_REAL_EMAILS

--- app/models/AumetUser.php
+++ app/models/AumetUser.php
@@ -10,16 +10,16 @@ class AumetUser extends BaseModel
         parent::__construct($dbConnectionAumet, 'production.User');
     }
 
-    public function getInchargePerson($companyID){
-        $query = 'SELECT * FROM production."User" where "CompanyID" = '.$companyID.' order by "IsAdmin" desc limit 1';
+    public function getInchargePerson($companyID)
+    {
+        $query = 'SELECT * FROM production."User" where "CompanyID" = ' . $companyID . ' order by "IsAdmin" desc limit 1';
         $arr = $this->db->exec($query);
 
         $objUser = count($arr) == 1 ? BaseModel::toObject($arr[0]) : null;
 
-        $isRealEmailsEnabled = (getenv('IS_BETA_ACCESS') == 1) && (getenv('ENV') == 'prod');
+        $isRealEmailsEnabled = (getenv('USE_REAL_EMAILS') == 1) && (getenv('ENV') == 'prod');
 
-        // TODO: remove for production
-        if($objUser != null && !$isRealEmailsEnabled){
+        if ($objUser != null && !$isRealEmailsEnabled) {
             $objUser->Email = "aumet.generic.user+$objUser->ID@gmail.com";
         }
 
@@ -28,11 +28,12 @@ class AumetUser extends BaseModel
         //return $this->db->exec($query);
     }
 
-    public function getByCompanyId($companyID){
-        $arr = parent::getWhere('"CompanyID"='.$companyID);
-        $isRealEmailsEnabled = (getenv('IS_BETA_ACCESS') == 1) && (getenv('ENV') == 'prod');
-        if(!$isRealEmailsEnabled) {
-            foreach ($arr as $objUser){
+    public function getByCompanyId($companyID)
+    {
+        $arr = parent::getWhere('"CompanyID"=' . $companyID);
+        $isRealEmailsEnabled = (getenv('USE_REAL_EMAILS') == 1) && (getenv('ENV') == 'prod');
+        if (!$isRealEmailsEnabled) {
+            foreach ($arr as $objUser) {
                 $objUser->Email = "aumet.generic.user+$objUser->ID@gmail.com";
             }
         }
@@ -40,37 +41,33 @@ class AumetUser extends BaseModel
         return $arr;
     }
 
-    /**
-     * Get actual emails on ly if env is production and USE_REAL_EMAILS is set to 1
-     * else add user id at the end of aumet.generic.user
-     *
-     * @param $companyID
-     * @return array
-     */
-    public function getEmailListByCompanyId($companyID){
-        $arr = parent::getWhere('"CompanyID"='.$companyID);
+    public function getEmailListByCompanyId($companyID)
+    {
+        $arr = parent::getWhere('"CompanyID"=' . $companyID);
         $arrEmailList = [];
-        $isRealEmailsEnabled =  (getenv('USE_REAL_EMAILS') == 1) && (getenv('ENV') == 'prod');
-        if(!$isRealEmailsEnabled) {
+        $isRealEmailsEnabled = (getenv('USE_REAL_EMAILS') == 1) && (getenv('ENV') == 'prod');
+        if (!$isRealEmailsEnabled) {
             foreach ($arr as $objUser) {
-                $arrEmailList["aumet.generic.user+$objUser->ID@gmail.com"] = $objUser->FirstName.' '.$objUser->LastName;
+                $arrEmailList["aumet.generic.user+$objUser->ID@gmail.com"] = $objUser->FirstName . ' ' . $objUser->LastName;
             }
         } else {
             //return  $this->getInchargePerson($companyID)? (array) $this->getInchargePerson($companyID):[];
             foreach ($arr as $objUser) {
                 if (filter_var($objUser->Email, FILTER_VALIDATE_EMAIL)) {
-                    $arrEmailList[$objUser->Email] = $objUser->FirstName.' '.$objUser->LastName;
+                    $arrEmailList[$objUser->Email] = $objUser->FirstName . ' ' . $objUser->LastName;
                 }
             }
         }
         return $arrEmailList;
     }
 
-    public function getByEmail($email){
+    public function getByEmail($email)
+    {
         return parent::getByField('"Email"', $email);
     }
 
-    public function getById($id){
+    public function getById($id)
+    {
         return parent::getByField('"ID"', $id);
     }
 }
