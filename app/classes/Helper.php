fe888a1b7557a99f77aa2c044aeee18f03560279
Sajad Abbasi <sajjadintel@gmail.com>
2021-03-04 16:33:53 +0500

    ONEX-339

--- app/classes/Helper.php
+++ app/classes/Helper.php
@@ -13,4 +13,18 @@ class Helper {
         }
         return $ids;
     }
+
+    /**
+     * Convert date from one timezone to another one
+     */
+    function convertToOtherTimezone($datetime,$fromTimezone,$toTimezone){
+        $convertedDate = new DateTime($datetime, new DateTimeZone($fromTimezone));
+        $convertedDate->setTimezone(new DateTimeZone($toTimezone));
+        $date = $convertedDate->format("Y-m-d H:i:s A");
+        return [
+            'object'=>$convertedDate,
+            'date'  =>$date,
+        ];
+    }
+
 }
